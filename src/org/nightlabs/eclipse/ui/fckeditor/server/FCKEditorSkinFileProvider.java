4fc6a33045a05e0265564b1a3d91c3aa14e2d942
marc <marc@ad42c2b3-0219-42df-b61f-f0a99503bef8>
2008-05-18 18:17:56 +0000

    form colors
    git-svn-id: https://svn.nightlabs.org/svn/eclipse/trunk/org.nightlabs.eclipse.ui.fckeditor@83 ad42c2b3-0219-42df-b61f-f0a99503bef8

--- src/org/nightlabs/eclipse/ui/fckeditor/server/FCKEditorSkinFileProvider.java
+++ src/org/nightlabs/eclipse/ui/fckeditor/server/FCKEditorSkinFileProvider.java
@@ -7,7 +7,6 @@ import java.io.InputStream;
 import java.io.InputStreamReader;
 import java.util.Properties;
 
-import org.eclipse.ui.forms.FormColors;
 import org.nightlabs.eclipse.ui.fckeditor.IFCKEditor;
 
 /**
