23a4a0668255438be89483f314213c1a4a7e15b1
fitas_java <fitas_java@18c61ebf-f106-0410-9e1e-81fb82415b8f>
2008-06-16 10:41:24 +0000

    git-svn-id: https://svn.jfire.org/svn/main/trunk/org.nightlabs.base.ui@10898 18c61ebf-f106-0410-9e1e-81fb82415b8f

--- src/org/nightlabs/base/ui/util/RCPUtil.java
+++ src/org/nightlabs/base/ui/util/RCPUtil.java
@@ -236,8 +236,8 @@ public class RCPUtil
 	public static Image resize(Image image, int width, int height) {
 		Image scaled = new Image(Display.getDefault(), width, height);
 		GC gc = new GC(scaled);
-		gc.setAntialias(SWT.ON);
-		gc.setInterpolation(SWT.HIGH);
+		//gc.setAntialias(SWT.ON);
+		//gc.setInterpolation(SWT.HIGH);
 		gc.drawImage(image, 0, 0,
 				image.getBounds().width, image.getBounds().height,
 				0, 0, width, height);
