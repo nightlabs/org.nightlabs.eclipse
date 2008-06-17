08746785d6039081dc4cb1d4f795e33274cca0a8
fitas_java <fitas_java@18c61ebf-f106-0410-9e1e-81fb82415b8f>
2008-06-17 11:07:12 +0000

    git-svn-id: https://svn.jfire.org/svn/main/trunk/org.nightlabs.base.ui@10923 18c61ebf-f106-0410-9e1e-81fb82415b8f

--- src/org/nightlabs/base/ui/exceptionhandler/errorreport/ErrorReportWizardScreenShotPage.java
+++ src/org/nightlabs/base/ui/exceptionhandler/errorreport/ErrorReportWizardScreenShotPage.java
@@ -10,7 +10,9 @@ import org.eclipse.swt.widgets.Button;
 import org.eclipse.swt.widgets.Composite;
 import org.eclipse.swt.widgets.Control;
 import org.eclipse.swt.widgets.Display;
+import org.eclipse.swt.widgets.Event;
 import org.eclipse.swt.widgets.Label;
+import org.eclipse.swt.widgets.Listener;
 import org.nightlabs.base.ui.composite.XComposite;
 import org.nightlabs.base.ui.composite.XComposite.LayoutMode;
 import org.nightlabs.base.ui.util.RCPUtil;
@@ -25,8 +27,7 @@ public class ErrorReportWizardScreenShotPage extends DynamicPathWizardPage {
 	public ErrorReportWizardScreenShotPage() {
 
 		super(ErrorReportWizardScreenShotPage.class.getName(), "Send an error report"); 
-
-		// TODO Auto-generated constructor stub
+		// TODO Auto-generated constructor stub	
 	}
 
 	@Override
@@ -34,11 +35,16 @@ public class ErrorReportWizardScreenShotPage extends DynamicPathWizardPage {
 		// TODO Auto-generated method stub
 
 		XComposite page = new XComposite(parent, SWT.NONE, LayoutMode.TIGHT_WRAPPER);
-		
+		page.addListener(SWT.Resize, new Listener() {
+			public void handleEvent(Event e) {
+				onShow();
+			}
+		});
+
 		Label titleLabel = new Label(page, SWT.WRAP);
 		titleLabel.setLayoutData(new GridData(GridData.FILL_HORIZONTAL));
 		titleLabel.setText("Attach a screenshot of the Error"); 
-		
+
 		screenshotImage = new Label(page, SWT.WRAP);
 		screenshotImage.setLayoutData(new GridData(GridData.FILL_BOTH));
 
@@ -46,13 +52,13 @@ public class ErrorReportWizardScreenShotPage extends DynamicPathWizardPage {
 		sendScreenShotCheckBox.setLayoutData(new GridData(GridData.FILL_HORIZONTAL));
 		sendScreenShotCheckBox.setText("Attach the Above ScreenShot in the Report");
 		sendScreenShotCheckBox.addSelectionListener(new SelectionListener() {
-		      public void widgetSelected(SelectionEvent arg0) {
-		    	  setIsSendscreenshotImage(!getIsSendsScreenshotImage());
-		      }
+			public void widgetSelected(SelectionEvent arg0) {
+				setIsSendscreenshotImage(!getIsSendsScreenshotImage());
+			}
 
-		      public void widgetDefaultSelected(SelectionEvent arg0) {
-		      }
-		    });		    
+			public void widgetDefaultSelected(SelectionEvent arg0) {
+			}
+		});		    
 
 		ErrorReportSenderCfMod cfMod = Config.sharedInstance().createConfigModule(ErrorReportSenderCfMod.class);
 		sendScreenShotCheckBox.setSelection(cfMod.isAttachScreenShotToErrorReport_default());
@@ -64,7 +70,7 @@ public class ErrorReportWizardScreenShotPage extends DynamicPathWizardPage {
 			sendScreenShotCheckBox.setSelection(false);
 			sendScreenShotCheckBox.setToolTipText("you dont have enough access rights to use this feature!");
 		}		
-		
+
 		setIsSendscreenshotImage(sendScreenShotCheckBox.getSelection());
 		return page;
 	}
@@ -72,16 +78,19 @@ public class ErrorReportWizardScreenShotPage extends DynamicPathWizardPage {
 	@Override 
 	public void onShow()
 	{
-		ErrorReportWizard wizard = (ErrorReportWizard) getWizard();
-		ErrorReport errorReport = wizard.getErrorReport();
+		if(screenshotImage.getSize().x > 0 && screenshotImage.getSize().y > 0 )
+		{
+			ErrorReportWizard wizard = (ErrorReportWizard) getWizard();
+			ErrorReport errorReport = wizard.getErrorReport();
+			ImageData data = RCPUtil.convertToSWT(errorReport.getErrorScreenshot());				
 
-		ImageData data = RCPUtil.convertToSWT(errorReport.getErrorScreenshot());				
-		Image image = new Image(Display.getCurrent(), data);
+			Image image = new Image(Display.getCurrent(), data);
+			image = RCPUtil.resize(image,screenshotImage.getSize().x,screenshotImage.getSize().y,false);		
+			screenshotImage.setImage(image);
 
-		image = RCPUtil.resize(image,screenshotImage.getSize().x,screenshotImage.getSize().y,false);		
-		screenshotImage.setImage(image);
+		}
+
 	}
-
 	public Boolean getIsSendsScreenshotImage() {
 		return IsSendScreenshotImage;
 	}
