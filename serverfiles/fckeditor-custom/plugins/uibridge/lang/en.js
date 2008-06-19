cf445f18e01fa3d8d290d830805ebe852ace4b84
marco <marco@ad42c2b3-0219-42df-b61f-f0a99503bef8>
2008-06-19 12:54:14 +0000

    packed all content in subdirectory serverfiles into serverfiles.zip and removed it from SVN
    git-svn-id: https://svn.nightlabs.org/svn/eclipse/trunk/org.nightlabs.eclipse.ui.fckeditor@109 ad42c2b3-0219-42df-b61f-f0a99503bef8

--- serverfiles/fckeditor-custom/plugins/uibridge/lang/en.js
+++
@@ -1,4 +0,0 @@
-
-FCKLang['MyDlgBtnBrowseServer_Image'] = "Select Image...";
-FCKLang['MyDlgBtnBrowseServer_Link'] = "Select File...";
-FCKLang['MyDlgBtnBrowseServer_Flash'] = "Select Flash File...";
