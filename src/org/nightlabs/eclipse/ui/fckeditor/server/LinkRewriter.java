cc482e9864f806cbc75db4bbd0e9d3398cfc1caa
marc <marc@ad42c2b3-0219-42df-b61f-f0a99503bef8>
2008-06-27 11:45:18 +0000

    NPE fix
    git-svn-id: https://svn.nightlabs.org/svn/eclipse/trunk/org.nightlabs.eclipse.ui.fckeditor@119 ad42c2b3-0219-42df-b61f-f0a99503bef8

--- src/org/nightlabs/eclipse/ui/fckeditor/server/LinkRewriter.java
+++ src/org/nightlabs/eclipse/ui/fckeditor/server/LinkRewriter.java
@@ -13,6 +13,10 @@ public class LinkRewriter
 
 	public static String rewriteToPermaLinks(String html, String editorBaseUrl)
 	{
+		if(html == null)
+			return null;
+		if(editorBaseUrl == null)
+			throw new NullPointerException("editorBaseUrl");
 		Pattern p = Pattern.compile(Pattern.quote(editorBaseUrl+LOCALLINK_MIDFIX)+"(\\d+)");
 		Matcher matcher = p.matcher(html);
 		html = matcher.replaceAll(Matcher.quoteReplacement(PERMALINK_PREFIX)+"$1");
@@ -21,6 +25,10 @@ public class LinkRewriter
 
 	public static String rewriteToLocalLinks(String html, String editorBaseUrl)
 	{
+		if(html == null)
+			return null;
+		if(editorBaseUrl == null)
+			throw new NullPointerException("editorBaseUrl");
 		Pattern p = Pattern.compile(Pattern.quote(PERMALINK_PREFIX)+"(\\d+)");
 		Matcher matcher = p.matcher(html);
 		html = matcher.replaceAll(Matcher.quoteReplacement(editorBaseUrl+LOCALLINK_MIDFIX)+"$1");
