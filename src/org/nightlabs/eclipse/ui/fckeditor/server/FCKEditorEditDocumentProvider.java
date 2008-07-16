7c57a4cabd1c001b278dcbac3c00db20b4cb1a50
marc <marc@ad42c2b3-0219-42df-b61f-f0a99503bef8>
2008-07-16 15:55:27 +0000

    java script character unicode encoding
    git-svn-id: https://svn.nightlabs.org/svn/eclipse/trunk/org.nightlabs.eclipse.ui.fckeditor@126 ad42c2b3-0219-42df-b61f-f0a99503bef8

--- src/org/nightlabs/eclipse/ui/fckeditor/server/FCKEditorEditDocumentProvider.java
+++ src/org/nightlabs/eclipse/ui/fckeditor/server/FCKEditorEditDocumentProvider.java
@@ -32,7 +32,7 @@ import org.nightlabs.eclipse.ui.fckeditor.resource.Messages;
  * @author Marc Klinger - marc[at]nightlabs[dot]de
  * @version $Revision$ - $Date$
  */
-public class FCKEditorEditDocumentProvider extends BundleTemplateFileProvider 
+public class FCKEditorEditDocumentProvider extends BundleTemplateFileProvider
 {
 	public FCKEditorEditDocumentProvider(IFCKEditor editor) {
 		super(editor);
@@ -42,18 +42,19 @@ public class FCKEditorEditDocumentProvider extends BundleTemplateFileProvider
 	{
 		return Messages.getString("org.nightlabs.eclipse.ui.fckeditor.server.FCKEditorEditDocumentProvider.loadingText"); //$NON-NLS-1$
 	}
-	
+
 	private static String escapeContents(String contents)
 	{
 		if(contents == null)
 			return ""; //$NON-NLS-1$
-		return contents
-				.replace("\\", "\\\\") //$NON-NLS-1$ //$NON-NLS-2$
-				.replace("'", "\\'") //$NON-NLS-1$ //$NON-NLS-2$
-				.replace("\n", "\\n") //$NON-NLS-1$ //$NON-NLS-2$
-				.replace("\r", ""); //$NON-NLS-1$ //$NON-NLS-2$
+		return saveConvert(contents);
+//		return contents
+//		.replace("\\", "\\\\") //$NON-NLS-1$ //$NON-NLS-2$
+//		.replace("'", "\\'") //$NON-NLS-1$ //$NON-NLS-2$
+//		.replace("\n", "\\n") //$NON-NLS-1$ //$NON-NLS-2$
+//		.replace("\r", ""); //$NON-NLS-1$ //$NON-NLS-2$
 	}
-	
+
 	@Override
 	protected Map<String, String> getReplacements()
 	{
@@ -64,13 +65,97 @@ public class FCKEditorEditDocumentProvider extends BundleTemplateFileProvider
 		replacements.put("escapedHtml", escapeContents(content)); //$NON-NLS-1$
 		return replacements;
 	}
-	
+
 	/* (non-Javadoc)
 	 * @see org.nightlabs.eclipse.ui.fckeditor.server.FileProvider#getPath()
 	 */
 	@Override
-	public String getPath() 
+	public String getPath()
 	{
 		return "/edit.html"; //$NON-NLS-1$
 	}
+
+
+
+
+	// TAKEN FROM java.util.Properties
+	/*
+	 * Converts unicodes to encoded &#92;uxxxx and escapes
+	 * special characters with a preceding slash
+	 */
+	private static String saveConvert(String theString) {
+
+		boolean escapeUnicode = true;
+
+		int len = theString.length();
+		int bufLen = len * 2;
+		if (bufLen < 0) {
+			bufLen = Integer.MAX_VALUE;
+		}
+		StringBuilder outBuffer = new StringBuilder(bufLen);
+
+		for(int x=0; x<len; x++) {
+			char aChar = theString.charAt(x);
+			// Handle common case first, selecting largest block that
+			// avoids the specials below
+			if ((aChar > 62) && (aChar < 127)) {
+				if (aChar == '\\') {
+					outBuffer.append('\\'); outBuffer.append('\\');
+					continue;
+				}
+				outBuffer.append(aChar);
+				continue;
+			}
+			switch(aChar) {
+			case '\t':outBuffer.append('\\'); outBuffer.append('t');
+				break;
+			case '\n':outBuffer.append('\\'); outBuffer.append('n');
+				break;
+			case '\r':outBuffer.append('\\'); outBuffer.append('r');
+				break;
+			case '\f':outBuffer.append('\\'); outBuffer.append('f');
+				break;
+			// special:
+			case '>': // Fall through
+			case '<': // Fall through
+			case '"': // Fall through
+			case '\'':
+				appendUnicodeHex(outBuffer, aChar);
+				break;
+			default:
+				if (((aChar < 0x0020) || (aChar > 0x007e)) & escapeUnicode ) {
+					appendUnicodeHex(outBuffer, aChar);
+				} else {
+					outBuffer.append(aChar);
+				}
+			}
+		}
+		return outBuffer.toString();
+	}
+
+	private static void appendUnicodeHex(StringBuilder outBuffer, char aChar)
+	{
+		outBuffer.append('\\');
+		outBuffer.append('u');
+		outBuffer.append(toHex((aChar >> 12) & 0xF));
+		outBuffer.append(toHex((aChar >>  8) & 0xF));
+		outBuffer.append(toHex((aChar >>  4) & 0xF));
+		outBuffer.append(toHex( aChar        & 0xF));
+	}
+
+	// TAKEN FROM java.util.Properties
+	/**
+	 * Convert a nibble to a hex character
+	 * @param	nibble	the nibble to convert.
+	 */
+	private static char toHex(int nibble) {
+		return hexDigit[(nibble & 0xF)];
+	}
+
+	// TAKEN FROM java.util.Properties
+	/** A table of hex digits */
+	private static final char[] hexDigit = {
+		'0','1','2','3','4','5','6','7','8','9','A','B','C','D','E','F'
+	};
+
 }
