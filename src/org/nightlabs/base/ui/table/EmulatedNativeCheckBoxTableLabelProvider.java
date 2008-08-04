ce1b4282b2775c0690ea85efad8d0e9ee68d1881
tobias <tobias@18c61ebf-f106-0410-9e1e-81fb82415b8f>
2008-08-04 09:03:51 +0000

    Minor change.
    git-svn-id: https://svn.jfire.org/svn/main/trunk/org.nightlabs.base.ui@11626 18c61ebf-f106-0410-9e1e-81fb82415b8f

--- src/org/nightlabs/base/ui/table/EmulatedNativeCheckBoxTableLabelProvider.java
+++ src/org/nightlabs/base/ui/table/EmulatedNativeCheckBoxTableLabelProvider.java
@@ -4,12 +4,13 @@ import org.eclipse.jface.resource.JFaceResources;
 import org.eclipse.jface.viewers.ITableLabelProvider;
 import org.eclipse.jface.viewers.TableViewer;
 import org.eclipse.swt.SWT;
+import org.eclipse.swt.graphics.Color;
 import org.eclipse.swt.graphics.GC;
 import org.eclipse.swt.graphics.Image;
+import org.eclipse.swt.graphics.ImageData;
 import org.eclipse.swt.graphics.Point;
 import org.eclipse.swt.widgets.Button;
 import org.eclipse.swt.widgets.Shell;
-import org.nightlabs.base.ui.resource.Messages;
 import org.nightlabs.base.ui.util.JFaceUtil;
 
 /**
@@ -31,24 +32,60 @@ implements ITableLabelProvider
     }
   }
 
-  private Image makeShot(Shell shell, boolean type) {
-    Shell s = new Shell(shell,SWT.NO_TRIM);
-    Button b = new Button(s,SWT.CHECK);
-    b.setSelection(type);
-    Point bsize = b.computeSize(SWT.DEFAULT, SWT.DEFAULT);
-    b.setSize(bsize);
-    b.setLocation(0, 0);
-    s.setSize(bsize);
-    s.open();
+  private Image makeShot(Shell _shell, boolean type) {
+  	// Hopefully no platform uses exactly this color because we'll make
+		// it transparent in the image.
+		Color greenScreen = new Color(_shell.getDisplay(), 222, 223, 224);
 
-    GC gc = new GC(b);
-    Image image = new Image(shell.getDisplay(), bsize.x, bsize.y);
-    gc.copyArea(image, 0, 0);
-    gc.dispose();
+		Shell shell = new Shell(_shell, SWT.NO_TRIM);
 
-    s.close();
+		// otherwise we have a default gray color
+		shell.setBackground(greenScreen);
 
-    return image;
+		Button button = new Button(shell, SWT.CHECK);
+		button.setBackground(greenScreen);
+		button.setSelection(type);
+
+		// otherwise an image is located in a corner
+		button.setLocation(1, 1);
+		Point bsize = button.computeSize(SWT.DEFAULT, SWT.DEFAULT);
+
+		// otherwise an image is stretched by width
+		bsize.x = Math.max(bsize.x - 1, bsize.y - 1);
+		bsize.y = Math.max(bsize.x - 1, bsize.y - 1);
+		button.setSize(bsize);
+		shell.setSize(bsize);
+
+		shell.open();
+		GC gc = new GC(shell);
+		Image image = new Image(shell.getDisplay(), bsize.x, bsize.y);
+		gc.copyArea(image, 0, 0);
+		gc.dispose();
+		shell.close();
+
+		ImageData imageData = image.getImageData();
+		imageData.transparentPixel = imageData.palette.getPixel(greenScreen
+			.getRGB());
+
+		return new Image(_shell.getDisplay(), imageData);
+		
+//    Shell s = new Shell(shell,SWT.NO_TRIM);
+//    Button b = new Button(s,SWT.CHECK);
+//    b.setSelection(type);
+//    Point bsize = b.computeSize(SWT.DEFAULT, SWT.DEFAULT);
+//    b.setSize(bsize);
+//    b.setLocation(0, 0);
+//    s.setSize(bsize);
+//    s.open();
+//
+//    GC gc = new GC(b);
+//    Image image = new Image(shell.getDisplay(), bsize.x, bsize.y);
+//    gc.copyArea(image, 0, 0);
+//    gc.dispose();
+//
+//    s.close();
+//
+//    return image;
   }
 
 	public Image getCheckBoxImage(boolean checkState) {
