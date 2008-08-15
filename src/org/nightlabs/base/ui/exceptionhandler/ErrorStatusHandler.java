93a23277d8ffc6a1de730d9376e177601d5d6a58
nozkiller <nozkiller@18c61ebf-f106-0410-9e1e-81fb82415b8f>
2008-08-15 16:11:41 +0000

    made Statushandler style aware
    git-svn-id: https://svn.jfire.org/svn/main/trunk/org.nightlabs.base.ui@11812 18c61ebf-f106-0410-9e1e-81fb82415b8f

--- src/org/nightlabs/base/ui/exceptionhandler/ErrorStatusHandler.java
+++ src/org/nightlabs/base/ui/exceptionhandler/ErrorStatusHandler.java
@@ -3,9 +3,11 @@
  */
 package org.nightlabs.base.ui.exceptionhandler;
 
+import org.apache.log4j.Logger;
 import org.eclipse.core.runtime.IStatus;
 import org.eclipse.ui.application.WorkbenchAdvisor;
 import org.eclipse.ui.statushandlers.StatusAdapter;
+import org.eclipse.ui.statushandlers.StatusManager;
 import org.eclipse.ui.statushandlers.WorkbenchErrorHandler;
 import org.nightlabs.base.ui.app.AbstractWorkbenchAdvisor;
 
@@ -22,6 +24,8 @@ import org.nightlabs.base.ui.app.AbstractWorkbenchAdvisor;
  */
 public class ErrorStatusHandler extends WorkbenchErrorHandler {
 
+	private static final Logger logger = Logger.getLogger(ErrorStatusHandler.class);
+	
 	/**
 	 * Create a new {@link ErrorStatusHandler} 
 	 */
@@ -37,11 +41,16 @@ public class ErrorStatusHandler extends WorkbenchErrorHandler {
 	@Override
 	public void handle(StatusAdapter statusAdapter, int style) {
 		IStatus status = statusAdapter.getStatus();
-		if (status.getException() != null) {
-			// if the status has an exception set, we let the ExceptionHandlerRegistry handle
-			if (ExceptionHandlerRegistry.syncHandleException(status.getException())) {
-				return;
-			}
+		if ((style & (StatusManager.BLOCK | StatusManager.SHOW)) > 0) {
+			if (status.getException() != null) {
+				// if the status has an exception set, we let the ExceptionHandlerRegistry handle
+				if (ExceptionHandlerRegistry.syncHandleException(status.getException())) {
+					return;
+				}
+			}			
+		}
+		else if ((style & StatusManager.LOG) > 0) {
+			logger.error("Exception with style StatusManager.LOG", status.getException());
 		}
 		super.handle(statusAdapter, style);
 	}
