372eb8927fafc3a4cbcee79e9a50fb6feecec0fe
frederik <frederik@ad42c2b3-0219-42df-b61f-f0a99503bef8>
2008-09-02 13:31:14 +0000

    git-svn-id: https://svn.nightlabs.org/svn/eclipse/trunk/org.nightlabs.eclipse.ui.pdfviewer@148 ad42c2b3-0219-42df-b61f-f0a99503bef8

---
+++ src/org/nightlabs/eclipse/ui/pdfviewer/util/Utilities.java
@@ -0,0 +1,8 @@
+package org.nightlabs.eclipse.ui.pdfviewer.util;
+
+public class Utilities {
+	public static int doubleToInt(double pageProperty) {
+		return (int)(Math.ceil(pageProperty));
+	}
+
+} 
