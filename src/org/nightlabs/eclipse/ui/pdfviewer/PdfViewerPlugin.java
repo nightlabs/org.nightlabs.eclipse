ee71a17155b731c5417f1a1eed55fea977891b47
marco <marco@ad42c2b3-0219-42df-b61f-f0a99503bef8>
2008-09-17 11:55:24 +0000

    added svn:keywords
    git-svn-id: https://svn.nightlabs.org/svn/eclipse/trunk/org.nightlabs.eclipse.ui.pdfviewer@243 ad42c2b3-0219-42df-b61f-f0a99503bef8

--- src/org/nightlabs/eclipse/ui/pdfviewer/PdfViewerPlugin.java
+++ src/org/nightlabs/eclipse/ui/pdfviewer/PdfViewerPlugin.java
@@ -5,6 +5,10 @@ import org.osgi.framework.BundleContext;
 
 /**
  * The activator class controls the plug-in life cycle
+ *
+ * @version $Revision$ - $Date$
+ * @author frederik löser - frederik at nightlabs dot de
+ * @author marco schulze - marco at nightlabs dot de
  */
 public class PdfViewerPlugin extends AbstractUIPlugin {
 
@@ -13,7 +17,7 @@ public class PdfViewerPlugin extends AbstractUIPlugin {
 
 	// The shared instance
 	private static PdfViewerPlugin plugin;
-	
+
 	/**
 	 * The constructor
 	 */
@@ -24,6 +28,7 @@ public class PdfViewerPlugin extends AbstractUIPlugin {
 	 * (non-Javadoc)
 	 * @see org.eclipse.ui.plugin.AbstractUIPlugin#start(org.osgi.framework.BundleContext)
 	 */
+	@Override
 	public void start(BundleContext context) throws Exception {
 		super.start(context);
 		plugin = this;
@@ -33,6 +38,7 @@ public class PdfViewerPlugin extends AbstractUIPlugin {
 	 * (non-Javadoc)
 	 * @see org.eclipse.ui.plugin.AbstractUIPlugin#stop(org.osgi.framework.BundleContext)
 	 */
+	@Override
 	public void stop(BundleContext context) throws Exception {
 		plugin = null;
 		super.stop(context);
