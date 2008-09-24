12be53fa360d86eeff0c6d48f4e5789432edade4
frederik <frederik@ad42c2b3-0219-42df-b61f-f0a99503bef8>
2008-09-24 15:16:23 +0000

    * added some fix for stack overflow error (sadly still occurring sometimes) * renamed variables in PDF viewer editor
    git-svn-id: https://svn.nightlabs.org/svn/eclipse/trunk/org.nightlabs.eclipse.ui.pdfviewer@271 ad42c2b3-0219-42df-b61f-f0a99503bef8

--- src/org/nightlabs/eclipse/ui/pdfviewer/internal/PdfViewerComposite.java
+++ src/org/nightlabs/eclipse/ui/pdfviewer/internal/PdfViewerComposite.java
@@ -1,10 +1,13 @@
 package org.nightlabs.eclipse.ui.pdfviewer.internal;
 
 import java.awt.BorderLayout;
+import java.awt.EventQueue;
 import java.awt.Frame;
 import java.awt.Graphics;
 import java.awt.Graphics2D;
 import java.awt.Panel;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 import java.awt.event.ComponentAdapter;
 import java.awt.event.ComponentEvent;
 import java.awt.event.FocusAdapter;
@@ -21,10 +24,13 @@ import java.awt.geom.Point2D;
 import java.awt.geom.Rectangle2D;
 import java.beans.PropertyChangeListener;
 import java.beans.PropertyChangeSupport;
+import java.lang.reflect.Field;
+import java.lang.reflect.Method;
 import java.util.Collection;
 
 import javax.swing.JPanel;
 import javax.swing.SwingUtilities;
+import javax.swing.Timer;
 
 import org.apache.log4j.Logger;
 import org.eclipse.swt.SWT;
@@ -194,6 +200,19 @@ public class PdfViewerComposite extends Composite
 		scrollBarVertical = viewPanelComposite.getVerticalBar();
 		scrollBarHorizontal = viewPanelComposite.getHorizontalBar();
 		viewPanelFrame = SWT_AWT.new_Frame(viewPanelComposite);
+
+		// see https://bugs.eclipse.org/bugs/show_bug.cgi?id=171432
+		// TODO perhaps not the right place to call this as the bug still occurs sometimes
+		boolean x11ErrorHandlerFixInstalled = false;
+		if(!x11ErrorHandlerFixInstalled && "gtk".equals( SWT.getPlatform())) {
+			x11ErrorHandlerFixInstalled = true;
+			EventQueue.invokeLater( new Runnable() {
+			   public void run() {
+				   initX11ErrorHandlerFix();
+			   }
+			});
+		}
+
 		viewPanelFrame.setFocusableWindowState(true);
 //		viewPanelFrame.setFocusable(true);
 
@@ -911,19 +930,70 @@ public class PdfViewerComposite extends Composite
 		throw new IllegalStateException("What the fucking shit?!");
     }
 
-	private boolean isPageVisible(Rectangle2D viewBounds, Rectangle2D pageBounds)
-	{
+	private boolean isPageVisible(Rectangle2D viewBounds, Rectangle2D pageBounds) {
 		return viewBounds.contains(pageBounds) || pageBounds.contains(viewBounds) || viewBounds.intersects(pageBounds);
 	}
 
 	private Point2DDouble zoomScreenResolutionFactor;
 
-	public Point2D getZoomScreenResolutionFactor()
-	{
+	public Point2D getZoomScreenResolutionFactor() {
 		return zoomScreenResolutionFactor;
 	}
 
 	public PdfDocument getPdfDocument() {
 		return pdfDocument;
 	}
+
+	private static void initX11ErrorHandlerFix() {
+		assert EventQueue.isDispatchThread();
+
+		try {
+			// get XlibWrapper.SetToolkitErrorHandler() and XSetErrorHandler() methods
+			Class xlibwrapperClass = Class.forName( "sun.awt.X11.XlibWrapper" );
+			final Method setToolkitErrorHandlerMethod = xlibwrapperClass.getDeclaredMethod( "SetToolkitErrorHandler", null );
+			final Method setErrorHandlerMethod = xlibwrapperClass.getDeclaredMethod( "XSetErrorHandler", new Class[] { Long.TYPE } );
+			setToolkitErrorHandlerMethod.setAccessible( true );
+			setErrorHandlerMethod.setAccessible( true );
+
+			// get XToolkit.saved_error_handler field
+			Class xtoolkitClass = Class.forName( "sun.awt.X11.XToolkit" );
+			final Field savedErrorHandlerField = xtoolkitClass.getDeclaredField( "saved_error_handler" );
+			savedErrorHandlerField.setAccessible( true );
+
+			// determine the current error handler and the value of XLibWrapper.ToolkitErrorHandler
+			// (XlibWrapper.SetToolkitErrorHandler() sets the X11 error handler to
+			// XLibWrapper.ToolkitErrorHandler and returns the old error handler)
+			final Object defaultErrorHandler = setToolkitErrorHandlerMethod.invoke( null, null );
+			final Object toolkitErrorHandler = setToolkitErrorHandlerMethod.invoke( null, null );
+			setErrorHandlerMethod.invoke( null, new Object[] { defaultErrorHandler } );
+
+			// create timer that watches XToolkit.saved_error_handler whether its value is equal
+			// to XLibWrapper.ToolkitErrorHandler, which indicates the start of the trouble
+			Timer timer = new Timer( 200, new ActionListener() {
+				public void actionPerformed( ActionEvent e ) {
+					try {
+						Object savedErrorHandler = savedErrorHandlerField.get( null );
+						if( toolkitErrorHandler.equals( savedErrorHandler ) ) {
+							// Last saved error handler in XToolkit.WITH_XERROR_HANDLER
+							// is XLibWrapper.ToolkitErrorHandler, which will cause
+							// the StackOverflowError when the next X11 error occurs.
+							// Workaround: restore the default error handler.
+							// Also update XToolkit.saved_error_handler so that
+							// this is done only once.
+							setErrorHandlerMethod.invoke( null, new Object[] { defaultErrorHandler } );
+							savedErrorHandlerField.setLong( null, ((Long)defaultErrorHandler).longValue() );
+						}
+					} catch( Exception ex ) {
+						// ignore
+					}
+
+				}
+			} );
+			timer.start();
+		} catch( Exception ex ) {
+			// ignore
+		}
+	}
+
+
 }
