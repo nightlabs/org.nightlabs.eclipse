fb5cd4f64d4012766d1c3f6b4e546a81779e2c62
frederik <frederik@ad42c2b3-0219-42df-b61f-f0a99503bef8>
2008-10-08 14:09:09 +0000

    * added documentation in PdfPrinter
    git-svn-id: https://svn.nightlabs.org/svn/eclipse/trunk/org.nightlabs.eclipse.ui.pdfviewer.extension@326 ad42c2b3-0219-42df-b61f-f0a99503bef8

--- src/org/nightlabs/eclipse/ui/pdfviewer/extension/printer/PdfPrinter.java
+++ src/org/nightlabs/eclipse/ui/pdfviewer/extension/printer/PdfPrinter.java
@@ -29,6 +29,7 @@ import com.sun.pdfview.PDFFile;
 import com.sun.pdfview.PDFPage;
 import com.sun.pdfview.PDFRenderer;
 
+
 public class PdfPrinter implements DocumentPrinter {
 
 	private static final Logger logger = Logger.getLogger(PdfPrinter.class);
@@ -38,6 +39,10 @@ public class PdfPrinter implements DocumentPrinter {
 	private PrinterJob printerJob;
 	private PageFormat defaultPage;
 
+	/**
+	 * Gets a {@link PDFFile} from a {@link File} by using {@link PdfFileLoader}.
+	 * @param file the file to print
+	 */
 	@Override
 	public void printDocument(File file) throws PrinterException {
 		try {
@@ -47,17 +52,31 @@ public class PdfPrinter implements DocumentPrinter {
         	x.initCause(exception);
         	throw x;
         }
-        this.printDocument(pdfFile);
+        this.printDocument();
 	}
 
-	public void printDocument(PDFFile file) throws PrinterException {
+	/**
+	 * Creates {@link PrinterJob} and {@link PrintService} instances for printing and prints
+	 * the loaded {@link PDFFile} using a {@link PdfPageable}.
+	 */
+	public void printDocument() throws PrinterException {
 
+		// Creates and returns a PrinterJob which is initially associated with the default printer.
 	    printerJob = PrinterJob.getPrinterJob();
+
+	    // Returns a PrintService describing the capabilities of the printer given by the printerConfiguration.
+	    // The PrintService is also configured in compliance with the settings of this configuration.
 		PrintService printService = PrintUtil.getConfiguredPrintService(printerConfiguration, true);
+
+		// Associates the PrinterJob with the PrintService.
 		printerJob.setPrintService(printService);
+
+	    // Creates a new PageFormat instance and sets it to a default size and orientation.
 		defaultPage = printerJob.defaultPage();
+
 	    printerJob.setPageable(new PdfPageable());
 
+	    // Prints a set of pages (the PDF document).
 		printerJob.print();
 	}
 
@@ -73,20 +92,36 @@ public class PdfPrinter implements DocumentPrinter {
 
 	private class PdfPageable implements Pageable {
 
+		/**
+		 * Gets the number of pages of the given {@link PDFFile}.
+		 * @return the number of pages.
+		 */
 		@Override
         public int getNumberOfPages() {
 	        int numberOfPages = pdfFile.getNumPages();
 	        return numberOfPages;
         }
 
+		/**
+		 * Gets the {@link PageFormat} for the current page of the given {@link PDFFile}.
+		 * @param pageIndex the number of the current page (zero-based) to get the {@link PageFormat} for.
+		 * @return the PageFormat for the considered page.
+		 */
 		@Override
         public PageFormat getPageFormat(int pageIndex) throws IndexOutOfBoundsException {
-			PDFPage pdfPage = pdfFile.getPage(pageIndex + 1);
+
+			PDFPage pdfPage = pdfFile.getPage(pageIndex + 1);	// pageIndex is zero-based!
 			PageFormat pageFormat = new PageFormat();
 			Paper paper = new Paper();
+
+			// Sets the width and height of the Paper object to the measures of the default page (see above).
 			paper.setSize(defaultPage.getWidth(), defaultPage.getHeight());
+			// Sets the orientation of the PageFormat object to the one of the currently considered PDF page.
 			pageFormat.setOrientation(getOrientation(pdfPage));
+			// Sets the imageable area (the area in which printing occurs) of the Paper object according to the
+			// measures of the default page. The imageable area of the paper begins directly at the top-left point.
 			paper.setImageableArea(0, 0, defaultPage.getWidth(), defaultPage.getHeight());
+
 			pageFormat.setPaper(paper);
 
 			if (logger.isDebugEnabled()) {
@@ -99,9 +134,13 @@ public class PdfPrinter implements DocumentPrinter {
 			}
 
 			return pageFormat;
-
         }
 
+		/**
+		 * Gets a {@link Printable} for the current page of the given {@link PDFFile}.
+		 * @param pageIndex the number of the current page (zero-based) to get the {@link Printable} for.
+		 * @return a {@link Printable} for the considered page.
+		 */
 		@Override
         public Printable getPrintable(int pageIndex) throws IndexOutOfBoundsException {
 			if (pdfPrintable == null)
@@ -111,15 +150,31 @@ public class PdfPrinter implements DocumentPrinter {
 	}
 
 	private class PdfPrintable implements Printable {
+
+		/**
+		 * Prints the page at the specified index (zero-based) into the specified {@link Graphics} context in the specified format.
+		 * @param graphics the context into which the page is drawn.
+		 * @param pageFormat the {@link PageFormat} (size and orientation) of the current page to be drawn.
+		 * @param pageIndex the number of the current page to be drawn (zero-based).
+		 * @return PAGE_EXISTS if the page is rendered successfully or NO_SUCH_PAGE if <code>pageIndex</code>
+		 * specifies a non-existent page.
+		 */
 		@Override
 		public int print(Graphics graphics, PageFormat pageFormat, int pageIndex) throws PrinterException {
 
-			double widthFactor = 0;
-			double heightFactor = 0;
-			double minFactor;
+			/**
+			 * The factors widthScaleFactor and heightScaleFactor are used for scaling the given {@link PDFPage} to fit the paper dimensions
+			 * described by the default page. Only the smallest factor minScaleFactor of both will be taken into consideration for scaling.
+			 */
+			double widthScaleFactor = 0;
+			double heightScaleFactor = 0;
+			double minScaleFactor;
+
 			boolean needScaling = false;
 
-			Point2D.Double a4DimensionImage = new Point2D.Double(pageFormat.getWidth(), pageFormat.getHeight());
+
+			// Gets the dimensions of the current PageFormat (i.e. the dimensions of the default page).
+			Point2D.Double defaultPageDimensions = new Point2D.Double(pageFormat.getWidth(), pageFormat.getHeight());
 
 			if (logger.isDebugEnabled()) {
 				logger.debug("print: Print pageIndex " + pageIndex);
@@ -134,9 +189,12 @@ public class PdfPrinter implements DocumentPrinter {
 			int pageNumber = pageIndex + 1; 	// pageIndex is zero-based!
 			Graphics2D graphics2D = (Graphics2D)graphics;
 			PDFPage pdfPage = pdfFile.getPage(pageNumber);
+
+			// Gets the dimensions of the page bounds of the current PDF page before applying a screenResolutionFactor.
 			double pageWidth = pdfPage.getBBox().getWidth();
 			double pageHeight = pdfPage.getBBox().getHeight();
 
+			// Gets the screen resolution of default display
 			final Point[] screenDPI = new Point[1];
 			final Display display = Display.getDefault();
 			display.syncExec(new Runnable() {
@@ -144,49 +202,59 @@ public class PdfPrinter implements DocumentPrinter {
 	            	screenDPI[0] = display.getDPI();
 	            }
             });
-			Point2DDouble zoomScreenResolutionFactor = new Point2DDouble(
+
+			// Sets the screen resolution factor.
+			Point2DDouble screenResolutionFactor = new Point2DDouble(
 					(double)screenDPI[0].x / 72,
 					(double)screenDPI[0].y / 72
 			);
 
-			Point2D.Double pageBoundsDimensionImage = new Point2D.Double(
-					(pageWidth * zoomScreenResolutionFactor.getX()),
-					(pageHeight * zoomScreenResolutionFactor.getY())
+			// Sets the dimensions of the page bounds of the current PDF page after applying the computed screenResolutionFactor.
+			Point2D.Double pageBoundsImage = new Point2D.Double(
+					(pageWidth * screenResolutionFactor.getX()),
+					(pageHeight * screenResolutionFactor.getY())
 			);
 
 			if (logger.isDebugEnabled())
-				logger.debug("print: Page bounds before scaling: " + pageBoundsDimensionImage.x + " " + pageBoundsDimensionImage.y);
+				logger.debug("print: Page bounds before scaling: " + pageBoundsImage.x + " " + pageBoundsImage.y);
 
-			if (pageBoundsDimensionImage.x > a4DimensionImage.x) {
-				widthFactor = a4DimensionImage.x / pageBoundsDimensionImage.x;
+			// Tests if scaling has to be done to fit the current PDF page to the dimensions of default page.
+			if (pageBoundsImage.x > defaultPageDimensions.x) {
+				widthScaleFactor = defaultPageDimensions.x / pageBoundsImage.x;
 				needScaling = true;
 			}
-
-			if (pageBoundsDimensionImage.y > a4DimensionImage.y) {
-				heightFactor = a4DimensionImage.y / pageBoundsDimensionImage.y;
+			if (pageBoundsImage.y > defaultPageDimensions.y) {
+				heightScaleFactor = defaultPageDimensions.y / pageBoundsImage.y;
 				needScaling = true;
 			}
-
-			minFactor = Math.min(widthFactor, heightFactor);
-			if (logger.isDebugEnabled())
-				logger.debug("print: Scale-Factor: " + minFactor);
+			// Only the smallest factor of both factors will be taken into consideration and used for scaling.
+			minScaleFactor = Math.min(widthScaleFactor, heightScaleFactor);
 
 			if (needScaling) {
-				pageBoundsDimensionImage.x = pageBoundsDimensionImage.x * minFactor;
-				pageBoundsDimensionImage.y = pageBoundsDimensionImage.y * minFactor;
+				pageBoundsImage.x = pageBoundsImage.x * minScaleFactor;
+				pageBoundsImage.y = pageBoundsImage.y * minScaleFactor;
 			}
 
-			if (logger.isDebugEnabled())
-				logger.debug("print: Page bounds after scaling: " + pageBoundsDimensionImage.x + " " + pageBoundsDimensionImage.y);
+			if (logger.isDebugEnabled()) {
+				logger.debug("print: Scale-Factor: " + minScaleFactor);
+				logger.debug("print: Page bounds after scaling: " + pageBoundsImage.x + " " + pageBoundsImage.y);
+			}
 
+			// Creates a new PDFGraphics state, given a Graphics2D.
+			// The image bounds into which to fit the page are described by a rectangle using the (possibly scaled)
+			// page bounds as bounds.
+		    // A clipping will not be used as the whole page should be drawn.
 			PDFRenderer renderer = new PDFRenderer(	pdfPage,
 													graphics2D,
-													new Rectangle(	0,
-																	0,
-																	(int)(pageBoundsDimensionImage.x),
-																	(int)(pageBoundsDimensionImage.y)),
-													null,	// the whole page is drawn
-													null);
+													new Rectangle(
+														0,
+														0,
+														(int)(pageBoundsImage.x),
+														(int)(pageBoundsImage.y)
+													),
+													null,	// do not use any clipping
+													null	// do not use any background color
+			);
 
 			try {
 				pdfPage.waitForFinish();
@@ -195,6 +263,7 @@ public class PdfPrinter implements DocumentPrinter {
             	x.initCause(exception);
             	throw x;
             }
+
             renderer.run();
 
 			return Printable.PAGE_EXISTS;
@@ -217,6 +286,11 @@ public class PdfPrinter implements DocumentPrinter {
 //
 //    }
 
+	/**
+	 * Gets the orientation of the given PDF page.
+	 * @param pdfPage the PDF page to get the orientation from.
+	 * @return the orientation of the given PDF page.
+	 */
 	private int getOrientation(PDFPage pdfPage) {
 		float aspectRatio = pdfPage.getAspectRatio();
 		if (aspectRatio < 1) {
