7768eac6ec06b87ac0b28ba045fe8e7ab71348d8
marco <marco@18c61ebf-f106-0410-9e1e-81fb82415b8f>
2008-11-06 17:15:25 +0000

    Added cleanup-code in case DynamicPathWizardPage.createPageContents fails. This pretty reliably ensures integrity of the wizard.
    git-svn-id: https://svn.jfire.org/svn/main/trunk/org.nightlabs.base.ui@12744 18c61ebf-f106-0410-9e1e-81fb82415b8f

--- src/org/nightlabs/base/ui/wizard/DynamicPathWizardPage.java
+++ src/org/nightlabs/base/ui/wizard/DynamicPathWizardPage.java
@@ -26,16 +26,20 @@
 
 package org.nightlabs.base.ui.wizard;
 
+import java.util.Set;
+
+import org.apache.log4j.Logger;
 import org.eclipse.jface.resource.ImageDescriptor;
 import org.eclipse.jface.wizard.WizardPage;
 import org.eclipse.swt.widgets.Composite;
 import org.eclipse.swt.widgets.Control;
+import org.nightlabs.util.CollectionUtil;
 
 /**
  * Extends WizardPage and resolves its wickedness of NullPointerExceptions.
  * Subclass this instead of WizardPage and simply implement
  * {@link #createPageContents(Composite)}.
- * 
+ *
  * @author Alexander Bieber <alex[AT]nightlabs[DOT]de>
  * @author Marc Klinger - marc[at]nightlabs[dot]de
  */
@@ -43,8 +47,10 @@ public abstract class DynamicPathWizardPage
 extends WizardPage
 implements IDynamicPathWizardPage
 {
+	private static final Logger logger = Logger.getLogger(DynamicPathWizardPage.class);
+
 	protected Control contents;
-	
+
 	/**
 	 * Create a new DynamicPathWizardPage.
 	 * @param pageName The identifier used for the page
@@ -73,33 +79,59 @@ implements IDynamicPathWizardPage
 			ImageDescriptor titleImage) {
 		super(pageName, title, titleImage);
 	}
-	
+
 	/**
 	 * Overidden to prevent NullPointerExceptions.
-	 * 
+	 *
 	 * @see org.eclipse.jface.dialogs.IDialogPage#getControl()
 	 */
 	@Override
 	public Control getControl() {
 		return contents;
 	}
-	
+
 	/**
 	 * Calls createPageContents and then setContents() with the result.
 	 * Additionally set the message to {@link #getDefaultPageMessage()} if
 	 * overridden and does not return null.
-	 * 
+	 * <p>
+	 * {@inheritDoc}
+	 * </p>
+	 *
 	 * @see org.eclipse.jface.dialogs.IDialogPage#createControl(org.eclipse.swt.widgets.Composite)
 	 */
+	@Override
 	public void createControl(Composite parent) {
-		contents = createPageContents(parent);
+		// We had the situation that creating pages of a ClientPaymentProcessor failed and this screwed up the
+		// complete PaymentWizard (actually the payment summary page displayed the page that couldn't be created).
+		// Therefore, we track which children exist before calling createPageContents(...) and if an error occurs,
+		// we dispose all children that have been added by createPageContents(...) before failing.
+		// Marco.
+
+		Set<Control> childrenBefore = CollectionUtil.array2HashSet(parent.getChildren());
+		boolean successful = false;
+		try {
+			contents = createPageContents(parent);
+			successful = true;
+		} finally {
+			if (!successful) {
+				try {
+					for (Control child : parent.getChildren()) {
+						if (!childrenBefore.contains(child))
+							child.dispose();
+					}
+				} catch (Throwable t) {
+					logger.warn("createControl: cleaning up pageContents failed!", t);
+				}
+			}
+		}
 		setDefaultPageMessage();
 		setControl(contents);
 	}
-	
+
 	/**
 	 * Create the WizardPage contents and return the topmost Control.
-	 * 
+	 *
 	 * @param parent
 	 */
 	public abstract Control createPageContents(Composite parent);
@@ -108,14 +140,14 @@ implements IDynamicPathWizardPage
 	 * The implementation of this method in <tt>DynamicPathWizardPage</tt> returns
 	 * always <tt>true</tt>. If you want to forbid this page from being the
 	 * last, you must overwrite it.
-	 * 
+	 *
 	 * @see org.nightlabs.base.ui.wizard.IDynamicPathWizardPage#canBeLastPage()
 	 */
 	public boolean canBeLastPage()
 	{
 		return true;
 	}
-	
+
 	/**
 	 * This method is called by {@link #updateStatus(String)}.
 	 * Override to set the message displayed initially and on
@@ -131,13 +163,13 @@ implements IDynamicPathWizardPage
 	protected String getDefaultPageMessage() {
 		return null;
 	}
-	
+
 	private void setDefaultPageMessage() {
 		String defaultMessage = getDefaultPageMessage();
 		if (defaultMessage != null)
 			setMessage(defaultMessage);
 	}
-	
+
 	/**
 	 * Lets you control the status with an error-message.
 	 * When null is passed the displayed message is set to
@@ -145,7 +177,7 @@ implements IDynamicPathWizardPage
 	 * will be set to true.
 	 * Otherwise pageComplete will be false and the
 	 * passed errMsg will be displayed.
-	 * 
+	 *
 	 * @param errMsg
 	 */
 	protected void updateStatus(String errMsg) {
@@ -168,7 +200,7 @@ implements IDynamicPathWizardPage
 	public boolean isShown() {
 		return shown;
 	}
-	
+
 	/**
 	 * @see org.nightlabs.base.ui.wizard.IDynamicPathWizardPage#onShow()
 	 */
@@ -182,19 +214,19 @@ implements IDynamicPathWizardPage
 	 */
 	public void onHide() {
 	}
-	
+
 	/**
 	 * @see org.nightlabs.base.ui.wizard.IDynamicPathWizardPage#onNext()
 	 */
 	public void onNext() {
 	}
-	
+
 	/**
 	 * @see org.nightlabs.base.ui.wizard.IDynamicPathWizardPage#onPrevious()
 	 */
 	public void onPrevious() {
 	}
-	
+
 	/**
 	 * @see org.nightlabs.base.ui.wizard.IDynamicPathWizardPage#onFinish()
 	 */
