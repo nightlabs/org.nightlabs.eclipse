f2e95f75787e90c65a1f2a0790461bf6b763b6fe
alex <alex@18c61ebf-f106-0410-9e1e-81fb82415b8f>
2008-11-12 18:05:42 +0000

    removed some comments
    git-svn-id: https://svn.jfire.org/svn/main/trunk/org.nightlabs.base.ui@12783 18c61ebf-f106-0410-9e1e-81fb82415b8f

--- src/org/nightlabs/base/ui/entity/editor/EntityEditorPageWithProgress.java
+++ src/org/nightlabs/base/ui/entity/editor/EntityEditorPageWithProgress.java
@@ -177,33 +177,14 @@ public abstract class EntityEditorPageWithProgress extends FormPage implements F
 		protected IStatus run(ProgressMonitor monitor) {
 			final IEntityEditorPageController controller = getPageController();
 			if (controller != null) {
-				// moved this to fillBody right after addSections
-//				controller.addModifyListener(new IEntityEditorPageControllerModifyListener() {
-//					public void controllerObjectModified(EntityEditorPageControllerModifyEvent modifyEvent) {
-//						handleControllerObjectModified(modifyEvent);
-//					}
-//				});
 
 				CompoundProgressMonitor compoundMonitor = new CompoundProgressMonitor(new ProgressMonitorWrapper(progressMonitorPart), monitor);
 				if (controller instanceof EntityEditorPageController) {
 					((EntityEditorPageController)controller).load(compoundMonitor);
-//					((EntityEditorPageController)controller).addModifyListener(new IEntityEditorPageControllerModifyListener() {
-//						public void controllerObjectModified(EntityEditorPageControllerModifyEvent modifyEvent) {
-//							handleControllerObjectModified(modifyEvent);
-//						}
-//					});
 				} // (controller instanceof EntityEditorPageController)
 				else
 					controller.doLoad(compoundMonitor);
-
-//				controller.addModifyListener(new IEntityEditorPageControllerModifyListener() {
-//					public void controllerObjectModified(EntityEditorPageControllerModifyEvent modifyEvent) {
-//						handleControllerObjectModified(modifyEvent);
-//					}
-//				});
 			}
-//			asyncCallback(); // TODO: Maybe change this and require the controller to fire change event onLoad() ?!?!
-			// TODO maybe call switchToContent() here?!?!
 			return Status.OK_STATUS;
 		}
 	};
