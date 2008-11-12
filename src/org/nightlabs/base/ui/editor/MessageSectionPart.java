98d72ce6d06cbfc87df850ff22d8f65584260b6b
alex <alex@18c61ebf-f106-0410-9e1e-81fb82415b8f>
2008-11-12 18:02:36 +0000

    message section part now handles description of underlying section
    git-svn-id: https://svn.jfire.org/svn/main/trunk/org.nightlabs.base.ui@12781 18c61ebf-f106-0410-9e1e-81fb82415b8f

--- src/org/nightlabs/base/ui/editor/MessageSectionPart.java
+++ src/org/nightlabs/base/ui/editor/MessageSectionPart.java
@@ -26,29 +26,72 @@
 package org.nightlabs.base.ui.editor;
 
 import org.eclipse.jface.dialogs.IMessageProvider;
+import org.eclipse.swt.SWT;
 import org.eclipse.swt.layout.GridData;
 import org.eclipse.swt.layout.GridLayout;
 import org.eclipse.swt.widgets.Composite;
+import org.eclipse.swt.widgets.Text;
 import org.eclipse.ui.forms.editor.IFormPage;
+import org.eclipse.ui.forms.widgets.ExpandableComposite;
 import org.eclipse.ui.forms.widgets.FormToolkit;
 import org.eclipse.ui.forms.widgets.Section;
 import org.nightlabs.base.ui.entity.editor.EntityEditorUtil;
 import org.nightlabs.base.ui.message.IMessageDisplayer;
+import org.nightlabs.base.ui.toolkit.IToolkit;
 
 /**
+ * {@link MessageSectionPart} creates a {@link Section} with a container for the section contents.
+ * The section will have get a {@link GridData} and therefore can be used inside {@link GridLayout}
+ * as is. The container for the sections contents will have a {@link GridLayout} with one column by default.
+ * <p>
+ * 
+ * </p>   
+ * 
  * @author Daniel.Mazurek [at] NightLabs [dot] de
- *
+ * @author Alexander Bieber <!-- alex [AT] nightlabs [DOT] de -->
  */
 public class MessageSectionPart
 extends RestorableSectionPart
 implements IMessageDisplayer
 {
+	private Composite container;
+	/**
+	 * We maintain our own description control because there was no
+	 * way of preventing the original description control of a 
+	 * Section to have borders 
+	 * (Section constructor causes reflow 
+	 * -> reflow causes XComposite to give all Texts a border painter)
+	 */
+	private Text descriptionControl;
+	
+	/**
+	 * Create a new {@link MessageSectionPart} with a Section and a container
+	 * {@link Composite} for the contents of this section.
+	 * 
+	 * @param page The page the new part should be in.
+	 * @param parent The parent for the new Section.
+	 * @param style The style of the new Section, use constants of {@link ExpandableComposite} and {@link Section} for that.
+	 * 		Note that using {@link Section#DESCRIPTION} will have no affect as it is stripped from the style, the description
+	 * 		control is managed by this class. 
+	 * @param title The title for the new section.
+	 */
 	public MessageSectionPart(IFormPage page, Composite parent, int style, String title) {
 		this(page.getEditor().getToolkit(), parent, style, title);
 	}
 	
+	/**
+	 * Create a new {@link MessageSectionPart} with a Section and a container
+	 * {@link Composite} for the contents of this section.
+	 * 
+	 * @param toolkit The toolkit to use.
+	 * @param parent The parent for the new Section.
+	 * @param style The style of the new Section, use constants of {@link ExpandableComposite} and {@link Section} for that.
+	 * 		Note that using {@link Section#DESCRIPTION} will have no affect as it is stripped from the style, the description
+	 * 		control is managed by this class. 
+	 * @param title The title for the new section.
+	 */
 	public MessageSectionPart(FormToolkit toolkit, Composite parent, int style, String title) {
-		super(parent, toolkit, style);
+		super(parent, toolkit, (style & ~Section.DESCRIPTION));
 		Section section = getSection();
 		section.setText(title);
 		section.setExpanded(true);
@@ -59,7 +102,13 @@ implements IMessageDisplayer
 		container.setLayoutData(new GridData(GridData.FILL_BOTH));
 	}
 
-	private Composite container;
+	
+	/**
+	 * Returns the container that was created for the contents of the underlying section
+	 * and that was set as the sections client.
+	 * 
+	 * @return The container for the contents of the underlying section.
+	 */
 	public Composite getContainer() {
 		return container;
 	}
@@ -84,5 +133,34 @@ implements IMessageDisplayer
 	public void setMessage(String message, int style) {
 		getManagedForm().getForm().getForm().setMessage(message, style);
 	}
-
+	
+	/**
+	 * Returns the description text of the underlying section.
+	 * Note that this method returns <code>null</code> as 
+	 * long as no description was set.
+	 * 
+	 * @return the description text of the underlying section.
+	 */
+	public String getDescription() {
+		return getSection().getDescription();
+	}
+	
+	/**
+	 * Set the description text for the underlying section.
+	 * Note that this might cause the description control to
+	 * be added to the section dynamically as it is added
+	 * when it is needed first.
+	 * 
+	 * @param description The description text to set.
+	 */
+	public void setDescription(String description) {
+		if (getSection().isDisposed())
+			return;
+		if (descriptionControl == null) {
+			descriptionControl = new Text(getSection(), SWT.READ_ONLY | SWT.WRAP);
+			getSection().setDescriptionControl(descriptionControl);
+			getSection().getDescriptionControl().setData(IToolkit.KEY_DRAW_BORDER, Boolean.FALSE);
+		}
+		getSection().setDescription(description);
+	}
 }
