68a6cb83a1365c9137b8395d41f3e331ba139898
nozkiller <nozkiller@18c61ebf-f106-0410-9e1e-81fb82415b8f>
2009-01-30 15:21:48 +0000

    restructured preference pages (https://bt.nightlabs.de//view.php?id=1057) added therefore a dependency to org.eclipse.preference.ui to org.nightlabs.base.ui
    git-svn-id: https://svn.jfire.org/svn/main/trunk/org.nightlabs.keyreader.ui@13276 18c61ebf-f106-0410-9e1e-81fb82415b8f

---
+++ src/org/nightlabs/keyreader/ui/preference/HardwarePreferencePage.java
@@ -0,0 +1,20 @@
+/**
+ *
+ */
+package org.nightlabs.keyreader.ui.preference;
+
+import org.nightlabs.base.ui.preference.CategoryPreferencePage;
+
+/**
+ * @author daniel[at]nightlabs[dot]de
+ *
+ */
+public class HardwarePreferencePage extends CategoryPreferencePage
+{
+	/**
+	 *
+	 */
+	public HardwarePreferencePage() {
+	}
+
+}
