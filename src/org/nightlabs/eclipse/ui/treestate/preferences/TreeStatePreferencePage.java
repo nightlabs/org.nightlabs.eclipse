9f3499f547d033d037b368d10c4c933e5817e7e1
chairatk <chairatk@ad42c2b3-0219-42df-b61f-f0a99503bef8>
2009-03-06 10:19:23 +0000

    modified the preferences page
    git-svn-id: https://svn.nightlabs.org/svn/eclipse/trunk/org.nightlabs.eclipse.ui.treestate@435 ad42c2b3-0219-42df-b61f-f0a99503bef8

--- src/org/nightlabs/eclipse/ui/treestate/preferences/TreeStatePreferencePage.java
+++ src/org/nightlabs/eclipse/ui/treestate/preferences/TreeStatePreferencePage.java
@@ -1,6 +1,7 @@
 package org.nightlabs.eclipse.ui.treestate.preferences;
 
 import org.eclipse.jface.preference.BooleanFieldEditor;
+import org.eclipse.jface.preference.FieldEditor;
 import org.eclipse.jface.preference.FieldEditorPreferencePage;
 import org.eclipse.jface.preference.IntegerFieldEditor;
 import org.eclipse.jface.util.PropertyChangeEvent;
@@ -14,42 +15,61 @@ implements IWorkbenchPreferencePage
 	private BooleanFieldEditor enableState; 
 	private IntegerFieldEditor relativeTime;
 	private IntegerFieldEditor absoluteTime;
-	
+
 	public TreeStatePreferencePage() {
 		super(GRID);
 		setPreferenceStore(Preferences.getPreferenceStore());
-		setTitle("Title");
 	}
-	
+
 	@Override
 	protected void createFieldEditors() {
 		enableState = new BooleanFieldEditor(Preferences.PREFERENCE_ENABLE_STATE,
 				"Enable",
 				getFieldEditorParent());
-		
+
 		relativeTime = new IntegerFieldEditor(Preferences.PREFERENCE_RELATIVE_TIME,
-				"Relative Time(seconds)",
+				"Relative time(1-100 seconds)",
 				getFieldEditorParent());
 		relativeTime.setValidRange(1, 100);
 		relativeTime.setTextLimit(3);
 		relativeTime.setStringValue(Preferences.getPreferenceStore().getString(
 				Preferences.PREFERENCE_RELATIVE_TIME));
-	
+
 		absoluteTime = new IntegerFieldEditor(Preferences.PREFERENCE_ABSOLUTE_TIME,
-				"Absolute Time(seconds)",
+				"Absolute time(1-100 seconds)",
 				getFieldEditorParent());
 		absoluteTime.setValidRange(1, 100);
 		absoluteTime.setTextLimit(3);
 		absoluteTime.setStringValue(Preferences.getPreferenceStore().getString(
 				Preferences.PREFERENCE_RELATIVE_TIME));
-		
+
 		addField(enableState);
 		addField(relativeTime);
 		addField(absoluteTime);
 	}
 
 	@Override
-	public void init(IWorkbench workbench) {
-		
+	public void propertyChange(PropertyChangeEvent event) {
+		if (event.getProperty().equals(FieldEditor.VALUE)) { 
+			if (event.getSource() == absoluteTime 
+					|| event.getSource() == relativeTime
+					|| event.getSource() == enableState) 
+				checkState(); 
+		} 
 	}
+	@Override
+	protected void checkState() {
+		super.checkState();
+		if (absoluteTime.getIntValue() < relativeTime.getIntValue()) {
+			setErrorMessage("The absolute time should have value greater than the relative time.");       
+			setValid(false); 
+		} 
+		else { 
+			setErrorMessage(null); 
+			setValid(true); 
+		} 
+	}
+
+	@Override
+	public void init(IWorkbench workbench) {}
 }
