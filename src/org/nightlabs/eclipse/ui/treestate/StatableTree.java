06543d98f0b8be53e7c80980c0d17c9ed3955b94
chairatk <chairatk@ad42c2b3-0219-42df-b61f-f0a99503bef8>
2009-03-06 12:41:10 +0000

    added some javadocs
    git-svn-id: https://svn.nightlabs.org/svn/eclipse/trunk/org.nightlabs.eclipse.ui.treestate@437 ad42c2b3-0219-42df-b61f-f0a99503bef8

--- src/org/nightlabs/eclipse/ui/treestate/StatableTree.java
+++ src/org/nightlabs/eclipse/ui/treestate/StatableTree.java
@@ -2,14 +2,23 @@ package org.nightlabs.eclipse.ui.treestate;
 
 import org.eclipse.swt.widgets.Tree;
 
+/**
+ * This interface is used for the {@link TreeStateController} that needs to get an identifier 
+ * of the tree by calling {@link StatableTree#getID()} and the {@link StatableTree#getTree} is used
+ * for getting the tree from the container that contains the tree.
+ *
+ * @author Chairat Kongarayawetchakun - chairat[at]nightlabs[dot]de
+ */
 public interface StatableTree
 {
 	/**
+	 * Returns the ID for identifying the tree (MyClass.getClass().getName()) for example).
 	 * @return String identifier of the Tree
 	 */
 	String getID();
 
 	/**
+	 * Returns the tree.
 	 * @return Tree
 	 */
 	Tree getTree();
