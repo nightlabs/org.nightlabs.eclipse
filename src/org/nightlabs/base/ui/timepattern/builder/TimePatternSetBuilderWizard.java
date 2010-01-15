8761c492d4c4b4d8fff5ea4b1610ccf842170141
alex <alex@18c61ebf-f106-0410-9e1e-81fb82415b8f>
2010-01-15 17:37:08 +0000

    Removed deprecated usage of WizardDialog
    git-svn-id: https://svn.jfire.org/svn/main/trunk/org.nightlabs.base.ui@16662 18c61ebf-f106-0410-9e1e-81fb82415b8f

--- src/org/nightlabs/base/ui/timepattern/builder/TimePatternSetBuilderWizard.java
+++ src/org/nightlabs/base/ui/timepattern/builder/TimePatternSetBuilderWizard.java
@@ -4,6 +4,8 @@
 package org.nightlabs.base.ui.timepattern.builder;
 
 import org.eclipse.jface.window.Window;
+import org.eclipse.swt.widgets.Shell;
+import org.nightlabs.base.ui.util.RCPUtil;
 import org.nightlabs.base.ui.wizard.DynamicPathWizard;
 import org.nightlabs.base.ui.wizard.DynamicPathWizardDialog;
 import org.nightlabs.timepattern.TimePatternFormatException;
@@ -52,13 +54,23 @@ public class TimePatternSetBuilderWizard extends DynamicPathWizard {
 		return true;
 	}
 	
-	public static boolean open(TimePatternSet timePatternSet) {
+	public static boolean open(Shell shell, TimePatternSet timePatternSet) {
 		TimePatternSetBuilderWizard wiz = new TimePatternSetBuilderWizard(timePatternSet);
 		wiz.addBuilderHop(new DailyTimePatternBuilderHop());
 		wiz.addBuilderHop(new MonthlyTimePatternBuilderHop());
 		wiz.addBuilderHop(new SingleExecTimePatternBuilderHop());
-		DynamicPathWizardDialog dlg = new DynamicPathWizardDialog(wiz);
+		DynamicPathWizardDialog dlg = new DynamicPathWizardDialog(shell, wiz);
 		return dlg.open() == Window.OK;
+	}
+
+	/**
+	 * @deprecated Don't use this method as {@link RCPUtil#getActiveShell()} is used by this method
+	 *             that might return a wrong shell. Better use {@link #open(Shell, TimePatternSet)}
+	 *             and pass the shell from the ui this wizard was triggered from.
+	 */
+	@Deprecated
+	public static boolean open(TimePatternSet timePatternSet) {
+		return open(RCPUtil.getActiveShell(), timePatternSet);
 	}
 
 }
