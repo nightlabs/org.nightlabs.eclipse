5a1db153361221d2e6bd962a6c3d1e791e793af1
alex <alex@18c61ebf-f106-0410-9e1e-81fb82415b8f>
2010-02-05 17:20:12 +0000

    Added active-checkbox support to InputTimePatternComposite
    git-svn-id: https://svn.jfire.org/svn/main/trunk/org.nightlabs.base.ui@16836 18c61ebf-f106-0410-9e1e-81fb82415b8f

--- src/org/nightlabs/base/ui/timepattern/input/InputTimePatternEditComposite.java
+++ src/org/nightlabs/base/ui/timepattern/input/InputTimePatternEditComposite.java
@@ -1,11 +1,15 @@
 package org.nightlabs.base.ui.timepattern.input;
 
+import org.eclipse.core.runtime.ListenerList;
 import org.eclipse.swt.SWT;
+import org.eclipse.swt.events.ModifyEvent;
+import org.eclipse.swt.events.ModifyListener;
 import org.eclipse.swt.events.SelectionAdapter;
 import org.eclipse.swt.events.SelectionEvent;
 import org.eclipse.swt.layout.GridData;
 import org.eclipse.swt.widgets.Button;
 import org.eclipse.swt.widgets.Composite;
+import org.eclipse.swt.widgets.Control;
 import org.eclipse.swt.widgets.Label;
 import org.eclipse.swt.widgets.Text;
 import org.nightlabs.base.ui.composite.XComposite;
@@ -15,10 +19,22 @@ import org.nightlabs.timepattern.InputTimePattern;
  * Composite to edit an {@link InputTimePattern} using an {@link InputTimePatternDialog}. It will
  * show a Label with the definition-string of the current time pattern and let the user open an
  * {@link InputTimePatternDialog} to create a new pattern.
+ * <p>
+ * This Composite is capable of optionally displaying a caption label or a caption checkbox. To
+ * create the checkbox use the style {@link #STYLE_SHOW_ACTIVE_CHECKBOX} when constructing the
+ * Composite. Note, that when the checkbox-style is applied, the 'active'-behaviour is switched on
+ * and {@link #isActive()} and {@link #setActive(boolean)} can be used.
+ * </p>
  * 
  * @author Alexander Bieber <!-- alex [AT] nightlabs [DOT] de -->
  */
 public class InputTimePatternEditComposite extends XComposite {
+
+	/**
+	 * This style can be used in addition to the SWT style to have the caption rendered as checkbox
+	 * and enabling the 'active'-behaviour.
+	 */
+	public static final int STYLE_SHOW_ACTIVE_CHECKBOX = 0x10000000;
 	
 	/** The {@link InputTimePattern} of this composite */
 	private InputTimePattern inputTimePattern;
@@ -27,6 +43,30 @@ public class InputTimePatternEditComposite extends XComposite {
 	
 	/** Optional caption, set in the constructor. If set a label will be created */
 	private final String caption;
+	
+	/** Listeners for modification */
+	private ListenerList modifyListeners = new ListenerList();
+	
+	/** Used to suppress unnecessary Modify notifications, set in {@link #setInputTimePattern(InputTimePattern)} */
+	private boolean refreshing = false;
+	/** ModifyListener on the displayLabel that delegates to all {@link ModifyListener}s added to this Composite */
+	private ModifyListener displayTextModifyListener = new ModifyListener() {
+		@Override
+		public void modifyText(ModifyEvent event) {
+			if (!refreshing) {
+				notifyModifyListener(event);
+			}
+		}
+	};
+	
+	/** The style this was constructed with */
+	private int constructorStyle;
+
+	/** The Checkbox created when {@link #STYLE_SHOW_ACTIVE_CHECKBOX} was used */
+	private Button activeCheckbox;
+
+	/** The Button opening the dialog to define the pattern */
+	private Button dialogButton;
 	
 	/**
 	 * Create a new {@link InputTimePatternEditComposite} that fills horizontal.
@@ -69,11 +109,31 @@ public class InputTimePatternEditComposite extends XComposite {
 	 * @param caption An optional caption for the Composite 
 	 */
 	public InputTimePatternEditComposite(Composite parent, int style, LayoutDataMode layoutDataMode, String caption) {
-		super(parent, style, LayoutMode.TIGHT_WRAPPER, layoutDataMode);
+		super(parent, getClearStyle(style), LayoutMode.TIGHT_WRAPPER, layoutDataMode);
+		this.constructorStyle = style;
 		this.caption = caption;
 		init();
 	}
+	
+	/**
+	 * Used internally to get the .
+	 * 
+	 * @param style The input style.
+	 * @return The SWT style stripped from the custom style.
+	 */
+	private static int getClearStyle(int style) {
+		return style & ~STYLE_SHOW_ACTIVE_CHECKBOX;
+	}
 
+	/**
+	 * Used internally to check for the {@link #STYLE_SHOW_ACTIVE_CHECKBOX} flag in the consturctorStyle.
+	 * 
+	 * @return <code>true</code> if the {@link #STYLE_SHOW_ACTIVE_CHECKBOX} was used.
+	 */
+	private boolean isShowActiveCheckbox() {
+		return (constructorStyle & STYLE_SHOW_ACTIVE_CHECKBOX) > 0;
+	}
+	
 	/**
 	 * Creates the widgets and listeners.
 	 */
@@ -81,28 +141,46 @@ public class InputTimePatternEditComposite extends XComposite {
 		getGridLayout().numColumns = 2;
 		getGridLayout().makeColumnsEqualWidth = false;
 		if (caption != null && !caption.isEmpty()) {
-			Label l = new Label(this, SWT.WRAP);
-			l.setText(caption);
+			
+			Control control;
+			if (isShowActiveCheckbox()) {
+				activeCheckbox = new Button(this, SWT.CHECK);
+				activeCheckbox.setText(caption);
+				activeCheckbox.addSelectionListener(new SelectionAdapter() {
+					@Override
+					public void widgetSelected(SelectionEvent e) {
+						activeSelected();
+					}
+				});				
+				control = activeCheckbox;
+			}
+			else {
+				Label l = new Label(this, SWT.WRAP);
+				l.setText(caption);
+				control = l;
+			}
 			GridData lgd = new GridData(GridData.FILL_HORIZONTAL);
 			lgd.horizontalSpan = 2;
-			l.setLayoutData(lgd);
+			control.setLayoutData(lgd);
 		}
 		displayText = new Text(this, getBorderStyle());
 		displayText.setEditable(false);
 		displayText.setLayoutData(new GridData(GridData.FILL_HORIZONTAL));
+		displayText.addModifyListener(displayTextModifyListener);
 		updateLabel();
-		final Button dlgButton = new Button(this, SWT.PUSH);
-		dlgButton.setText("...");
-		dlgButton.addSelectionListener(new SelectionAdapter() {
+		dialogButton = new Button(this, SWT.PUSH);
+		dialogButton.setText("...");
+		dialogButton.addSelectionListener(new SelectionAdapter() {
 			@Override
 			public void widgetSelected(SelectionEvent e) {
-				InputTimePattern dlgResult = InputTimePatternDialog.open(dlgButton.getShell(), inputTimePattern);
+				InputTimePattern dlgResult = InputTimePatternDialog.open(dialogButton.getShell(), inputTimePattern);
 				if (dlgResult != null) {
 					inputTimePattern = dlgResult;
 					updateLabel();
 				}
 			}
 		});
+		activeSelected();
 	}
 
 	/**
@@ -122,19 +200,101 @@ public class InputTimePatternEditComposite extends XComposite {
 	 * @param inputTimePattern The {@link InputTimePattern} to set.
 	 */
 	public void setInputTimePattern(InputTimePattern inputTimePattern) {
-		this.inputTimePattern = inputTimePattern;
-		getDisplay().syncExec(new Runnable() {
-			public void run() {
-				updateLabel();
-			}
-		});
+		refreshing = true;
+		try {
+			this.inputTimePattern = inputTimePattern;
+			getDisplay().syncExec(new Runnable() {
+				public void run() {
+					updateLabel();
+				}
+			});
+		} finally {
+			refreshing = false;
+		}
 	}
 
 	/**
-	 * @return the current {@link InputTimePattern}, might be <code>null</code> if not yet initialized.
+	 * Returns the current {@link InputTimePattern} as set or defined by the user. Note that if this
+	 * widget is not 'active', i.e. if {@link #isActive()} returns <code>false</code>, this method
+	 * will return <code>null</code>.
+	 * 
+	 * @return the current {@link InputTimePattern}, might be <code>null</code>.
 	 */
 	public InputTimePattern getInputTimePattern() {
-		return inputTimePattern;
+		if (isActive())
+			return inputTimePattern;
+		return null;
 	};
+
+	/**
+	 * Add a ModifyListener to changes of the {@link InputTimePattern} this composite will return.
+	 * Note that the event the {@link ModifyListener} receive might be <code>null</code>, and so it
+	 * should not be accessed, but rather the {@link InputTimePatternEditComposite} itself.
+	 * 
+	 * TODO alex think about this and maybe an own listener type is better.
+	 * 
+	 * @param modifyListener The ModifyListener to add.
+	 */
+	public void addModifyListener(ModifyListener modifyListener) {
+		modifyListeners.add(modifyListener);
+	}
+	
+	/**
+	 * Remove the given {@link ModifyListener} from the list of listeners.
+	 * 
+	 * @param modifyListener The event to remove.
+	 */
+	public void removeModifyListener(ModifyListener modifyListener) {
+		modifyListeners.remove(modifyListener);
+	}
+	
+	/**
+	 * Notify all {@link ModifyListener}s of this object of a change.
+	 * @param event The event, might be <code>null</code>
+	 */
+	protected void notifyModifyListener(ModifyEvent event) {
+		Object[] listeners = modifyListeners.getListeners();
+		for (Object listener : listeners) {
+			if (listener instanceof ModifyListener) {
+				((ModifyListener) listener).modifyText(event);
+			}
+		}
+	}
+
+	/**
+	 * Checks the activation of the widget. Returns <code>true</code> when
+	 * {@link #STYLE_SHOW_ACTIVE_CHECKBOX} was not used, otherwise it will check the selection-state
+	 * of the checkbox controlling the enablement and return its value. Note that, if
+	 * {@link #isActive()} returns <code>false</code> {@link #getInputTimePattern()} will be
+	 * <code>null</code>
+	 * 
+	 * @return Whether this widget is active an the user can define an InputTimePattern.
+	 */
+	public boolean isActive() {
+		return activeCheckbox == null ? true : activeCheckbox.getSelection();
+	}
+
+	/**
+	 * If the style {@link #STYLE_SHOW_ACTIVE_CHECKBOX} was used, this method can be used to control
+	 * the widgets activation-state. If a Composite is de-activated it will return <code>null</code>
+	 * (=not defined) when asked for {@link #getInputTimePattern()}.
+	 * 
+	 * @param active Whether the widget should be active or not.
+	 */
+	public void setActive(boolean active) {
+		if (this.activeCheckbox== null)
+			return;
+
+		this.activeCheckbox.setSelection(active);
+		activeSelected();
+	}
+	
+	/**
+	 * Used internally to update enablement.
+	 */
+	private void activeSelected() {
+		displayText.setEnabled(isActive());
+		dialogButton.setEnabled(isActive());
+	}
 
 }
