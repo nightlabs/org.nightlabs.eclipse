2919c10baad9782a2298b9b842f763f5f36975b3
frederik <frederik@18c61ebf-f106-0410-9e1e-81fb82415b8f>
2011-12-21 20:42:49 +0000

    * adjusted header-specific menu (now includes table-specific entries) * fixed "short column widths" bug when initially storing widths (stored too early)
    git-svn-id: https://svn.jfire.org/svn/main/trunk/org.nightlabs.base.ui@20111 18c61ebf-f106-0410-9e1e-81fb82415b8f

--- src/org/nightlabs/base/ui/table/column/config/TableColumnConfigurator.java
+++ src/org/nightlabs/base/ui/table/column/config/TableColumnConfigurator.java
@@ -1,6 +1,5 @@
 package org.nightlabs.base.ui.table.column.config;
 
-import java.util.ArrayList;
 import java.util.List;
 import java.util.Map;
 import java.util.StringTokenizer;
@@ -16,7 +15,6 @@ import org.eclipse.swt.events.DisposeEvent;
 import org.eclipse.swt.events.DisposeListener;
 import org.eclipse.swt.events.SelectionAdapter;
 import org.eclipse.swt.events.SelectionEvent;
-import org.eclipse.swt.graphics.Image;
 import org.eclipse.swt.graphics.Point;
 import org.eclipse.swt.graphics.Rectangle;
 import org.eclipse.swt.widgets.Display;
@@ -69,13 +67,13 @@ public class TableColumnConfigurator {
 	private boolean performedExternalCall;
 
 	/** Keeps track of the menu items available if the context menu is opened on a table entry. */
-	private List<MenuItemWrapper> entrySpecificMenuItems;
+//	private List<MenuItemWrapper> entrySpecificMenuItems;
 
 	/** True in the case the context menu has not been opened yet. */
-	private boolean pristine = true;
+//	private boolean pristine = true;
 
 	/** Keeps track whether the header context menu has been recently opened or not. */
-	private boolean headerSpecificMenuItemsSet;
+	private boolean headerSpecificMenuItemsAvailable;
 
 	/** The column configuration adapter interface. */
 	private ITableColumnConfigurationAdapter adapter;
@@ -99,6 +97,8 @@ public class TableColumnConfigurator {
 	 * current width of the considered table column.
 	 */
 	private final long threshold = 1000;
+	
+	private boolean debugging = false;	// if set to true cleans table-specific preference store
 
 	/**
 	 * The constructor.
@@ -109,13 +109,16 @@ public class TableColumnConfigurator {
 		this.table = adapter.getTable();
 
 		model = new TableColumnConfigurationModel();
-		entrySpecificMenuItems = new ArrayList<MenuItemWrapper>();
+//		entrySpecificMenuItems = new ArrayList<MenuItemWrapper>();
 
 		table.addListener(SWT.MenuDetect, new TableMenuDetectListener());
 		table.addDisposeListener(new TableDisposeListener());
 
 		for (int i = 0; i < table.getColumns().length; i++)
 			table.getColumn(i).addControlListener(new ColumnControlListener(i, adapter.getColumnIDs().get(i)));
+		
+		if (debugging)
+			PREFERENCE_STORE.putValue(buildUpPreferenceKey(adapter.getTableID()), "");	// for testing only
 
 		performColumnConfiguration();
 	}
@@ -123,40 +126,40 @@ public class TableColumnConfigurator {
 	/**
 	 * Wrapper for menu items that constitute to the table entries' context menu. Note, this is a workaround that has
 	 * a minor bug.
-	 * TODO resolve workaround
+	 * TODO resolve menu workaround
 	 * @author Frederik Loeser <!-- frederik [AT] nightlabs [DOT] de -->
 	 */
-	public class MenuItemWrapper {
-		private String text;
-		private Listener[] listeners;
-		private boolean enabled;
-		private Image image;
-		private int style;
-
-		public MenuItemWrapper(final String text, final Listener[] listeners, final boolean enabled, final Image image, final int style) {
-			this.text = text;
-			this.listeners = listeners;
-			this.enabled = enabled;
-			this.image = image;
-			this.style = style;
-		}
-
-		public String getText() {
-			return text;
-		}
-		public Listener[] getListeners() {
-			return listeners;
-		}
-		public boolean isEnabled() {
-			return enabled;
-		}
-		public Image getImage() {
-			return image;
-		}
-		public int getStyle() {
-			return style;
-		}
-	}
+//	public class MenuItemWrapper {
+//		private String text;
+//		private Listener[] listeners;
+//		private boolean enabled;
+//		private Image image;
+//		private int style;
+//
+//		public MenuItemWrapper(final String text, final Listener[] listeners, final boolean enabled, final Image image, final int style) {
+//			this.text = text;
+//			this.listeners = listeners;
+//			this.enabled = enabled;
+//			this.image = image;
+//			this.style = style;
+//		}
+//
+//		public String getText() {
+//			return text;
+//		}
+//		public Listener[] getListeners() {
+//			return listeners;
+//		}
+//		public boolean isEnabled() {
+//			return enabled;
+//		}
+//		public Image getImage() {
+//			return image;
+//		}
+//		public int getStyle() {
+//			return style;
+//		}
+//	}
 
 	/**
 	 * Initialises the model and additionally configures columns in the case the overview is opened at least
@@ -245,9 +248,8 @@ public class TableColumnConfigurator {
 	/**
 	 * Listener implementation registered for the table that will get notified when an event of type SWT.MenuDetect occurs.
 	 * Dependent on whether the user selected the header or not available menu items will differ. In the first case there
-	 * will be two entries, one for hiding the currently selected column and one for opening a dialog to configure special
-	 * column settings like order and visibility. Note, at the moment a workaround is used to set these menu items (see
-	 * {@link TableColumnConfigurator.MenuItemWrapper}).
+	 * will be two additional entries, one for hiding the currently selected column and one for opening a dialog to configure 
+	 * special column settings like order and visibility. Note, at the moment a workaround is used to set these menu items.
 	 * @author Frederik Loeser <!-- frederik [AT] nightlabs [DOT] de -->
 	 */
 	private class TableMenuDetectListener implements Listener {
@@ -259,14 +261,14 @@ public class TableColumnConfigurator {
 		public void handleEvent(final Event event) {
 			final Menu menu = table.getMenu();
 
-			if (pristine) {
-				for (int i = 0; i < menu.getItemCount(); i++) {
-					final MenuItem item = menu.getItem(i);
-					entrySpecificMenuItems.add(new MenuItemWrapper(item.getText(), item.getListeners(SWT.Selection),
-						item.getEnabled(), item.getImage(), item.getStyle()));
-				}
-				pristine = !pristine;
-			}
+//			if (pristine) {
+//				for (int i = 0; i < menu.getItemCount(); i++) {
+//					final MenuItem item = menu.getItem(i);
+//					entrySpecificMenuItems.add(new MenuItemWrapper(item.getText(), item.getListeners(SWT.Selection),
+//						item.getEnabled(), item.getImage(), item.getStyle()));
+//				}
+//				pristine = !pristine;
+//			}
 
 			final Point point = Display.getCurrent().map(null, table, new Point(event.x, event.y));
 			final Rectangle clientArea = table.getClientArea();
@@ -274,76 +276,90 @@ public class TableColumnConfigurator {
 
 			if (header) {
 				// Remove all items and add header-specific ones.
-				for (int i = 0; i < menu.getItemCount(); i++) {
-					menu.getItem(i).dispose();
-					i--;
+//				for (int i = 0; i < menu.getItemCount(); i++) {
+//					menu.getItem(i).dispose();
+//					i--;
+//				}
+				
+				if (itemHide == null && itemConfigure == null) { 
+					itemHide = new MenuItem(menu, SWT.NONE, 0);
+					itemHide.setText(Messages.getString(
+						"org.nightlabs.base.ui.table.column.config.TableColumnConfigurator.TableMenuDetectListener.item1.text")); //$NON-NLS-1$
+					itemHide.setEnabled(model.getColumnIDsHidden().size() < model.getColumnIDsOrder().size() ? true : false);
+					itemHide.addSelectionListener(new SelectionAdapter() {
+						@Override
+						public void widgetSelected(final SelectionEvent e) {
+							// TODO bug when first hiding column (refresh bug?)
+							final int j = findColumnIdxToHide(point);
+							if (j > -1) {
+								final String columnID = adapter.getColumnIDs().get(j);
+								if (!model.getColumnIDsHidden().contains(columnID))	// should not be the case, but...
+									model.getColumnIDsHidden().add(columnID);
+								localWidthAdaptation = true;
+								table.getColumn(j).setWidth(0);
+								table.getColumn(j).setResizable(false);
+								localWidthAdaptation = false;
+								model.getColumnIDToVisibilityState().put(columnID, false);
+							}
+						}
+					});
+	
+					itemConfigure = new MenuItem(menu, SWT.NONE, 1);
+					itemConfigure.setText(Messages.getString(
+						"org.nightlabs.base.ui.table.column.config.TableColumnConfigurator.TableMenuDetectListener.item2.text")); //$NON-NLS-1$
+					itemConfigure.addSelectionListener(new SelectionAdapter() {
+						@Override
+						public void widgetSelected(final SelectionEvent e) {
+							final TableColumnConfigurationDialog dialog = new TableColumnConfigurationDialog(TableColumnConfigurator.this,
+								table.getShell());
+							if (dialog.open() == Window.OK) {
+								// Adapt table columns visibility state and order according to settings performed (see model).
+								configureColumns();
+							}
+						}
+					});
 				}
-				itemHide = new MenuItem(menu, SWT.NONE);
-				itemHide.setText(Messages.getString(
-					"org.nightlabs.base.ui.table.column.config.TableColumnConfigurator.TableMenuDetectListener.item1.text")); //$NON-NLS-1$
-				itemHide.setEnabled(model.getColumnIDsHidden().size() < model.getColumnIDsOrder().size() ? true : false);
-				itemHide.addSelectionListener(new SelectionAdapter() {
-					@Override
-					public void widgetSelected(final SelectionEvent e) {
-						final int j = findColumnIdxToHide(point);
-						if (j > -1) {
-							final String columnID = adapter.getColumnIDs().get(j);
-							if (!model.getColumnIDsHidden().contains(columnID))	// should not be the case, but...
-								model.getColumnIDsHidden().add(columnID);
-							localWidthAdaptation = true;
-							table.getColumn(j).setWidth(0);
-							table.getColumn(j).setResizable(false);
-							localWidthAdaptation = false;
-							model.getColumnIDToVisibilityState().put(columnID, false);
-						}
-					}
-				});
-
-				itemConfigure = new MenuItem(menu, SWT.NONE);
-				itemConfigure.setText(Messages.getString(
-					"org.nightlabs.base.ui.table.column.config.TableColumnConfigurator.TableMenuDetectListener.item2.text")); //$NON-NLS-1$
-				itemConfigure.addSelectionListener(new SelectionAdapter() {
-					@Override
-					public void widgetSelected(final SelectionEvent e) {
-						final TableColumnConfigurationDialog dialog = new TableColumnConfigurationDialog(TableColumnConfigurator.this,
-							table.getShell());
-						if (dialog.open() == Window.OK) {
-							// Adapt table columns visibility state and order according to settings performed (see model).
-							configureColumns();
-						}
-					}
-				});
-				headerSpecificMenuItemsSet = true;
+				
+//				for (int i = 2; i < menu.getItemCount(); i++)
+//					menu.getItem(i).setEnabled(false);	=> leads to same problem as before: how to restore enabled state later on?
+					
+				headerSpecificMenuItemsAvailable = true;
 			} else {
-				// Remove header-specific items if set and set entry-specific items again. If no header-specific items are set
-				// nothing happens.
-				// TODO dependent on the situation some menu items may not be enabled due to old state being read out
-				if (headerSpecificMenuItemsSet) {
-					headerSpecificMenuItemsSet = false;
+				// Dispose header-specific items if set. If no header-specific items are set nothing happens.
+				if (headerSpecificMenuItemsAvailable) {
+					headerSpecificMenuItemsAvailable = false;
 					// Remove header-specific items...
 					if (itemHide != null && itemConfigure != null) {
-						for (int i = 0; i < menu.getItemCount(); i++) {
+						for (int i = 1; i > -1; i--) {
 							final MenuItem item = menu.getItem(i);
-							if (!itemHide.isDisposed() && item.getText() == itemHide.getText()) {
-								item.dispose();
-								i--;
-								continue;
-							}
-							if (!itemConfigure.isDisposed() && item.getText() == itemConfigure.getText()) {
-								item.dispose();
-								i--;
-							}
+							item.dispose();
 						}
+						itemHide = null;
+						itemConfigure = null;
+						
+//						for (int i = 0; i < menu.getItemCount(); i++) {
+//							final MenuItem item = menu.getItem(i);
+//							if (!itemHide.isDisposed() && item.getText() == itemHide.getText()) {
+//								item.dispose();
+//								itemHide = null;
+//								i--;
+//							}
+//							if (!itemConfigure.isDisposed() && item.getText() == itemConfigure.getText()) {
+//								item.dispose();
+//								itemConfigure = null;
+//								break;
+//							}
+//						}
 					}
 					// ...and set entry-specific items again.
-					for (final MenuItemWrapper wrapper : entrySpecificMenuItems) {
-						final MenuItem item = new MenuItem(menu, wrapper.getStyle());
-						item.setEnabled(wrapper.isEnabled());
-						item.setImage(wrapper.getImage());
-						item.setText(wrapper.getText());
-						for (final Listener listener : wrapper.getListeners())
-							item.addListener(SWT.Selection, listener);
-					}
+//					for (final MenuItemWrapper wrapper : entrySpecificMenuItems) {
+//						final MenuItem item = new MenuItem(menu, wrapper.getStyle());
+//						item.setEnabled(wrapper.isEnabled());
+//						item.setImage(wrapper.getImage());
+//						item.setText(wrapper.getText());
+//						for (final Listener listener : wrapper.getListeners())
+//							item.addListener(SWT.Selection, listener);
+//					}
 				}
 			}
 		}
@@ -373,6 +389,9 @@ public class TableColumnConfigurator {
 	 * the case the settings performed in column configuration dialog shall be applied.
 	 */
 	private void configureColumns() {
+		if (model.getColumnIDToColumnWidth().size() < table.getColumnCount())
+			updateColumnWidthsMap();
+			
 		final int[] order = table.getColumnOrder();
 		final int amountOfColumns = model.getColumnIDsOrder().size();
 		for (int i = 0; i < amountOfColumns; i++) {		// runs over all visible and hidden columns
@@ -391,8 +410,8 @@ public class TableColumnConfigurator {
 					} else if (model.getColumnIDsHidden().contains(columnID)) {
 						// ii) this column was hidden and will now be set visible again
 						table.getColumn(j).removeControlListener(hiddenColumnsControlListener);
-						table.getColumn(j).setWidth(100);		// TODO which value should be set here?
-						table.getColumn(j).setResizable(true);	// TODO which value should be set here?
+						table.getColumn(j).setWidth(100);
+						table.getColumn(j).setResizable(true);
 						model.getColumnIDsHidden().remove(columnID);
 					} else {
 						// iii) this column was neither hidden nor will it be hidden now
@@ -440,27 +459,44 @@ public class TableColumnConfigurator {
 					break;
 				}
 	}
+	
+	/**
+	 * Updates the column widths map as widths have not yet been set completely. This is most likely the case when the 
+	 * overview has been opened the very first time.
+	 */
+	private void updateColumnWidthsMap() {
+		for (int i = 0; i < table.getColumnCount(); i++) {
+			final String columnID = adapter.getColumnIDs().get(i);
+			model.getColumnIDToColumnWidth().put(columnID, table.getColumn(i).getWidth());
+		}
+	}
 
 	/**
 	 * Persists column configuration in the preference store.
 	 */
 	private void persistColumnConfiguration() {
-		final String value = createColumnConfigurationStateString();
-		PREFERENCE_STORE.putValue(buildUpPreferenceKey(PREFERENCE_KEY), value);
+		if (model.getColumnIDToColumnWidth().size() < table.getColumnCount())
+			updateColumnWidthsMap();
+		
+		final String value = debugging ? "" : createColumnConfigurationStateString();
+		String key = buildUpPreferenceKey(adapter.getTableID());
+		PREFERENCE_STORE.putValue(key, value);
 
-		if (LOGGER.isDebugEnabled())
-			LOGGER.debug("to be stored in preference store: " + value); //$NON-NLS-1$
+		if (LOGGER.isDebugEnabled()) {
+			LOGGER.debug("preference key: " + key);
+			LOGGER.debug("--------------> value: " + value); //$NON-NLS-1$
+		}
 	}
 
 	/**
-	 * Builds up a preference key using the table's ID and the given Strings as suffix successively. This is the key
-	 * under which column configuration will get persisted in the preference store.
+	 * Builds up a preference key using the given Strings as suffix successively (behind {@link #PREFERENCE_KEY}. 
+	 * This is the key under which column configuration will get persisted in the preference store.
 	 * @param suffixes The Strings to be appended to the key (separated by delimiter).
 	 * @return the complete preference key
 	 */
 	private String buildUpPreferenceKey(final String... suffixes) {
 		final StringBuilder sb = new StringBuilder();
-		sb.append(adapter.getTableID());
+		sb.append(PREFERENCE_KEY);
 
 		for (final String suffix : suffixes)
 			sb.append(PREFERENCE_KEY_DELIMITER).append(suffix);
@@ -488,9 +524,9 @@ public class TableColumnConfigurator {
 	}
 
 	/**
-	 * Initialises the model. If it is the first time the overview is opened, default values will get applied for certain
+	 * Initialises the model. If it is the first time ever the overview is opened, default values will get applied for certain
 	 * properties. Otherwise the model will get initialised with the values read out from preference store.
-	 * @return true in the case the overview has been opened the first time, false otherwise
+	 * @return true in the case the overview has been opened the first time ever, false otherwise
 	 */
 	private boolean initialiseModel() {
 		model.getColumnIDToVisibilityState().clear();
@@ -503,14 +539,17 @@ public class TableColumnConfigurator {
 			model.getColumnIDToColumnText().put(columnID, table.getColumn(i).getText());	// fix mapping, independent of visibility/ordering! e.g. 0->ID,1->Customer,...
 		}
 
-		final String prefValue = PREFERENCE_STORE.getString(buildUpPreferenceKey(PREFERENCE_KEY));
+		String key = buildUpPreferenceKey(adapter.getTableID());
+		final String value = PREFERENCE_STORE.getString(key);
+		
+		if (LOGGER.isDebugEnabled()) {
+			LOGGER.debug("preference key: " + key);
+			LOGGER.debug("--------------> value: " + value); //$NON-NLS-1$
+		}
 
-		if (LOGGER.isDebugEnabled())
-			LOGGER.debug("read out from preference store:   " + prefValue); //$NON-NLS-1$
-
-		if (!prefValue.equals("")) { //$NON-NLS-1$
+		if (!value.equals("")) { //$NON-NLS-1$
 			initialisation = false;
-			final StringTokenizer st1 = new StringTokenizer(prefValue, PREFERENCE_VALUE_MAIN_DELIMITER);
+			final StringTokenizer st1 = new StringTokenizer(value, PREFERENCE_VALUE_MAIN_DELIMITER);
 
 			while (st1.hasMoreTokens()) {
 				final String triple = st1.nextToken();	// (columnID,visibility,width)
@@ -532,16 +571,13 @@ public class TableColumnConfigurator {
 			return false;
 
 		} else {
-			// The overview has been opened the first time so there is no persisted state available.
+			// The overview has been opened the first time ever so there is no persisted state available.
 			for (int i = 0; i < table.getColumnCount(); i++) {
 				final String columnID = adapter.getColumnIDs().get(i);
 				model.getColumnIDToVisibilityState().put(columnID, true);	// all columns visible
 				model.getColumnIDsOrder().add(columnID);	// initial order, e.g. '0', '1', '2', '3',...
-				table.getColumn(i).pack();
-			}
-			for (int i = 0; i < table.getColumnCount(); i++) {
-				final String columnID = adapter.getColumnIDs().get(i);
-				model.getColumnIDToColumnWidth().put(columnID, table.getColumn(i).getWidth());
+				// Table columns have been drawn to a smaller scale if opened the 2nd time, so do not read out table column width
+				// here (too early; layout has not been fully set!) but when disposing table or finishing config dialog.
 			}
 			return true;
 		}
