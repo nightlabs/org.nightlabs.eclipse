5a92bc7c7d362971a51cbdecaff589242d9e6647
alex <alex@18c61ebf-f106-0410-9e1e-81fb82415b8f>
2012-02-28 13:05:29 +0000

    Removed JFire-dependency of keyreader.ui
    git-svn-id: https://svn.jfire.org/svn/main/trunk/org.nightlabs.keyreader.ui@20429 18c61ebf-f106-0410-9e1e-81fb82415b8f

--- src/org/nightlabs/keyreader/ui/preference/KeyReaderPreferencePage.java
+++ src/org/nightlabs/keyreader/ui/preference/KeyReaderPreferencePage.java
@@ -1,14 +1,18 @@
 package org.nightlabs.keyreader.ui.preference;
 
 import org.apache.log4j.Logger;
+import org.eclipse.jface.preference.PreferencePage;
 import org.eclipse.swt.SWT;
 import org.eclipse.swt.widgets.Composite;
+import org.eclipse.swt.widgets.Control;
+import org.eclipse.ui.IWorkbench;
+import org.eclipse.ui.IWorkbenchPreferencePage;
 import org.nightlabs.base.ui.composite.XComposite.LayoutDataMode;
-import org.nightlabs.jfire.base.ui.preferences.LSDPreferencePage;
 
 //TODO LSDPreferencePage should only be used when KeyReaderConfigModule comes from server not when used as lib in client
 public class KeyReaderPreferencePage
-extends LSDPreferencePage
+extends PreferencePage
+implements IWorkbenchPreferencePage
 {
 	static {
 		System.out.println("KeyReaderPreferencePage static initialiser called"); //$NON-NLS-1$
@@ -16,13 +20,6 @@ extends LSDPreferencePage
 
 	private static final Logger logger = Logger.getLogger(KeyReaderPreferencePage.class);
 	private KeyReaderPreferenceComposite keyReaderPreferenceComposite;
-
-	@Override
-	public void createPartContents(Composite parent)
-	{
-		keyReaderPreferenceComposite = new KeyReaderPreferenceComposite(parent, SWT.NONE, LayoutDataMode.NONE);
-		updateApplyButton();
-	}
 
 	@Override
 	public void createControl(Composite parent)
@@ -50,6 +47,17 @@ extends LSDPreferencePage
 		logger.debug("performOk"); //$NON-NLS-1$
 		keyReaderPreferenceComposite.save();
 		return super.performOk();
+	}
+
+	@Override
+	protected Control createContents(Composite parent) {
+		keyReaderPreferenceComposite = new KeyReaderPreferenceComposite(parent, SWT.NONE, LayoutDataMode.NONE);
+		updateApplyButton();
+		return keyReaderPreferenceComposite;
+	}
+
+	@Override
+	public void init(IWorkbench arg0) {
 	}
 
 }
